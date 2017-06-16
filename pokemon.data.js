4dd37ef9e574e6e4d4fdd35eeb40e1d5a9cd6c32
Luis Palomo <luispalomosiv@gmail.com>
2017-06-16 07:23:32 +0200

    Simple Express server with GET methods.

---
+++ pokemon.data.js
@@ -0,0 +1,35 @@
+exports.pokemonList = [
+    {
+        "name": "bulbasaur",
+        "weight": 69,
+        "sprites": {
+            "front_default": "https:\/\/raw.githubusercontent.com\/PokeAPI\/sprites\/master\/sprites\/pokemon\/1.png"
+        },
+        "height": 7,
+        "id": 1,
+        "base_experience": 64,
+        "types": ["grass", "poison"]
+    },
+    {
+        "name": "charmander",
+        "weight": 85,
+        "sprites": {
+            "front_default": "https:\/\/raw.githubusercontent.com\/PokeAPI\/sprites\/master\/sprites\/pokemon\/4.png"
+        },
+        "height": 6,
+        "id": 4,
+        "base_experience": 62,
+        "types": ["fire"]
+    },
+    {
+        "name": "squirtle",
+        "weight": 90,
+        "sprites": {
+            "front_default": "https:\/\/raw.githubusercontent.com\/PokeAPI\/sprites\/master\/sprites\/pokemon\/7.png"
+        },
+        "height": 5,
+        "id": 7,
+        "base_experience": 63,
+        "types": ["water"]
+    }
+];
